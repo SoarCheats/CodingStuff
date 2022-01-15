846a3aa81f0c91ceb1a9b5f72b25a16a85ffdf78
SoarsCheetos <70775273+SoarCheats@users.noreply.github.com>
2022-01-14 21:10:52 -0500

    Update FormMovable.cs

--- FormMovable.cs
+++ FormMovable.cs
@@ -1,12 +1,7 @@
-// Token: 0x06000001 RID: 1
 		[DllImport("user32.dll")]
 		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-
-		// Token: 0x06000002 RID: 2
 		[DllImport("user32.dll")]
 		public static extern bool ReleaseCapture();
-
-		// Token: 0x06000003 RID: 3 RVA: 0x00002050 File Offset: 0x00000250
 		private void FormMoveable_MouseDown(object sender, MouseEventArgs e)
 		{
 			bool flag = e.Button == MouseButtons.Left;
