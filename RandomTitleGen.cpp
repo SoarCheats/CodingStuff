7b03d3e9db311e998e760c779f2a04c8a5ae06dc
SoarsCheetos <70775273+SoarCheats@users.noreply.github.com>
2022-02-11 20:50:37 -0500

    Create RandomTitleGen.cpp

---
+++ RandomTitleGen.cpp
@@ -0,0 +1,9 @@
+std::string RandomString(const size_t length)
+{
+    std::string r;
+    static const char bet[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyzZ1234567890" };
+    srand((unsigned)time(NULL) * 5);
+    for (int i = 0; i < length; ++i)
+        r += bet[rand() % (sizeof(bet) - 1)];
+    return r;
+}
