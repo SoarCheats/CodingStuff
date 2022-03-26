4f9293b9e02c9b72fe48c305bce65a64027561a2
SoarCheats <70775273+SoarCheats@users.noreply.github.com>
2022-03-26 20:51:04 +0000

    Create UserControl.cs

---
+++ UserControl.cs
@@ -0,0 +1,7 @@
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            panel3.Controls.Clear();
+            panel3.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
